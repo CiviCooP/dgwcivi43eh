7ce3f0e61f3fa39360214d03543e424d8f71f9bd
Erik Hommel <hommel@ee-atwork.nl>
2013-04-17 16:43:22 +0200

    add data to config during install, to be checked and tested with Jaap

--- org.civicoop.dgwext/org.civicoop.dgw.custom/sql/dgwcustominstall.sql
+++ org.civicoop.dgwext/org.civicoop.dgw.custom/sql/dgwcustominstall.sql
@@ -6,3 +6,40 @@ CREATE  TABLE `dgw_config` (
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8
 COLLATE = utf8_general_ci;
+
+INSERT INTO `dgw_config` SET label = 'tabel data first', value = 'Aanvullende persoonsgegevens';
+INSERT INTO `dgw_config` SET label = 'persoonsnummer first', value = 'Persoonsnummer First'; 
+INSERT INTO `dgw_config` SET label = 'bsn', value = 'BSN';
+INSERT INTO `dgw_config` SET label = 'burgerlijke staat', value = 'Burgerlijke staat';
+
+INSERT INTO `dgw_config` SET label = 'tabel huurovereenkomst huishouden', value = 'Huurovereenkomst (huishouden)';
+INSERT INTO `dgw_config` SET label = 'hovnummer huishouden', value = 'HOV nummer First';
+INSERT INTO `dgw_config` SET label = 'vgenummer hov huishouden', value = 'VGE nummer First';
+INSERT INTO `dgw_config` SET label = 'hov adres huishouden', value = 'VGE adres First';
+INSERT INTO `dgw_config` SET label = 'begindatum hov huishouden', value = 'Begindatum HOV';
+INSERT INTO `dgw_config` SET label = 'einddatum hov huishouden', value = 'Einddatum HOV';
+INSERT INTO `dgw_config` SET label = 'naam hov huishouden', value = 'Correspondentienaam First';
+
+INSERT INTO `dgw_config` SET label = 'tabel koopovereenkomst', value = 'Koopovereenkomst';
+INSERT INTO `dgw_config` SET label = 'kovnummer', value = 'KOV nummer First';
+INSERT INTO `dgw_config` SET label = 'vgenummer kov', value = 'VGE nummer KOV';
+INSERT INTO `dgw_config` SET label = 'kov adres', value = 'VGE adres KOV';
+INSERT INTO `dgw_config` SET label = 'datum overdracht', value = 'Datum overdracht';
+INSERT INTO `dgw_config` SET label = 'naam kov', value = 'Correspondentienaam KOV';
+INSERT INTO `dgw_config` SET label = 'definitief', value = 'Definitief';
+INSERT INTO `dgw_config` SET label = 'type kov', value = 'Type KOV';
+INSERT INTO `dgw_config` SET label = 'verkoopprijs', value = 'Verkoopprijs';
+INSERT INTO `dgw_config` SET label = 'notaris', value = 'Notaris';
+INSERT INTO `dgw_config` SET label = 'taxatiewaarde', value = 'Taxatiewaarde';
+INSERT INTO `dgw_config` SET label = 'taxateur', value = 'Taxateur';
+INSERT INTO `dgw_config` SET label = 'taxatiedatum', value = 'Taxatiedatum';
+INSERT INTO `dgw_config` SET label = 'bouwkundige', value= 'Bouwkundige';
+INSERT INTO `dgw_config` SET label = 'datum bouwkeuring', value = 'Datum bouwkundige keuring';
+
+INSERT INTO `dgw_config` SET label = 'default burgerlijke staat', value = 'Onbekend';
+INSERT INTO `dgw_config` SET label = 'groep sync first', value = 'FirstSync';
+INSERT INTO `dgw_config` SET label = 'relatie hoofdhuurder', value = 'Hoofdhuurder';
+INSERT INTO `dgw_config` SET label = 'relatie medehuurder', value = 'Medehuurder';
+INSERT INTO `dgw_config` SET label = 'default location type', value = 'Thuis';
+INSERT INTO `dgw_config` SET label = 'helpdesk mail', value = 'helpdesk@degoedewoning.nl';
+
